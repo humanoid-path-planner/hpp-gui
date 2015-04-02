2408fbf1a10450ff17b1d86a4015fd40b6dc8b4d
Joseph Mirabel <jmirabel@laas.fr>
2015-04-02 15:33:01 +0200

    Configure main.cc to enable a proper use of QSettings

--- src/main.cc
+++ src/main.cc
@@ -4,6 +4,16 @@
 
 int main(int argc, char *argv[])
 {
+  QCoreApplication::setOrganizationName("@PROJECT_NAME@");
+  QCoreApplication::setOrganizationDomain("@PROJECT_URL@");
+  QCoreApplication::setApplicationName("@PROJECT_NAME@");
+  QCoreApplication::setApplicationVersion("@PROJECT_VERSION@");
+
+  QSettings::setPath(QSettings::IniFormat,
+                     QSettings::SystemScope, "@CMAKE_INSTALL_PREFIX@/etc");
+  QSettings::setPath(QSettings::NativeFormat,
+                     QSettings::SystemScope, "@CMAKE_INSTALL_PREFIX@/etc");
+
   QApplication a(argc, argv);
   a.setStyle(new QCleanlooksStyle);
   MainWindow w;
