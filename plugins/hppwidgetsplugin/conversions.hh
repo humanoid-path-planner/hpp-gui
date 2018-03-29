88ac6fc33cdf31b54159c1989ff3406072f1ca30
Joseph Mirabel <jmirabel@laas.fr>
2018-03-29 19:11:32 +0200

    Add some conversion functions.

--- plugins/hppwidgetsplugin/conversions.hh
+++ plugins/hppwidgetsplugin/conversions.hh
@@ -17,24 +17,44 @@ namespace hpp {
       v.set((type)t[0], (type)t[1], (type)t[2]);
     }
 
-    inline void fromHPP(const hpp::Transform__var& in, osgVector3& v)
+    inline void fromHPP(const hpp::Transform__slice* t, osgVector3& v)
     {
       typedef graphics::WindowsManager::value_type type;
-      const hpp::Transform__slice* t (in.in());
       v.set((type)t[0], (type)t[1], (type)t[2]);
+    }
+
+    inline void fromHPP(const hpp::Transform__var& in, osgVector3& v)
+    {
+      fromHPP (in.in(), v);
+    }
+
+    inline void fromHPP(const hpp::Transform__slice* t, osgQuat& q)
+    {
+      typedef graphics::WindowsManager::value_type type;
+      q.set((type)t[3], (type)t[4], (type)t[5], (type)t[6]);
     }
 
     inline void fromHPP(const hpp::Transform__var& in, osgQuat& q)
     {
-      typedef graphics::WindowsManager::value_type type;
-      const hpp::Transform__slice* t (in.in());
-      q.set((type)t[3], (type)t[4], (type)t[5], (type)t[6]);
+      fromHPP (in.in(), q);
+    }
+
+    inline void fromHPP(const hpp::Transform__slice* in, graphics::Configuration& c)
+    {
+      fromHPP(in, c.position);
+      fromHPP(in, c.quat);
     }
 
     inline void fromHPP(const hpp::Transform__var& in, graphics::Configuration& c)
     {
-      fromHPP(in, c.position);
-      fromHPP(in, c.quat);
+      fromHPP(in.in(), c);
+    }
+
+    inline void fromHPP(const hpp::TransformSeq& in, std::vector<graphics::Configuration>& c)
+    {
+      c.resize (in.length());
+      for (std::size_t i = 0; i < in.length(); ++i)
+        fromHPP (in[(CORBA::ULong)i], c[i]);
     }
 
   } // namespace gui
