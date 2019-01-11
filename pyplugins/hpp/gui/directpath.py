f90b70a5bd4a1459bbcf5fc89fdbd353563a8c9b
Joseph Mirabel <jmirabel@laas.fr>
2019-01-11 15:16:26 +0100

    Add button Apply constraints

--- pyplugins/hpp/gui/directpath.py
+++ pyplugins/hpp/gui/directpath.py
@@ -19,6 +19,15 @@ class DirectPathBox(QtGui.QGroupBox):
         q = self.plugin.client.robot.shootRandomConfig()
         self.plugin.hppPlugin.setCurrentQtConfig(q)
 
+    def applyConstraints (self):
+        q0 = self.plugin.hppPlugin.getCurrentQtConfig()
+        res, q1, err = self.plugin.client.problem.applyConstraints (q0)
+        self.plugin.hppPlugin.setCurrentQtConfig(q1)
+        if not res:
+            self.plugin.main.logError ("Projection failed: " + str(err))
+        else:
+            self.plugin.main.logError ("Projection succeeded.")
+
     def getFrom (self):
         self.fromCfg = self.plugin.hppPlugin.getCurrentQtConfig()
 
@@ -46,6 +55,8 @@ class DirectPathBox(QtGui.QGroupBox):
         box = QtGui.QVBoxLayout(self)
         random =  QtGui.QPushButton(self)
         box.addWidget(random)
+        applyConstraints =  QtGui.QPushButton(self)
+        box.addWidget(applyConstraints)
         setFrom =  QtGui.QPushButton(self)
         box.addWidget(setFrom)
         setTo =  QtGui.QPushButton(self)
@@ -57,12 +68,14 @@ class DirectPathBox(QtGui.QGroupBox):
         makePath = QtGui.QPushButton(self)
         box.addWidget(makePath)
         random.text = "Shoot random config"
+        applyConstraints.text = "Apply constraints"
         setFrom.text = 'Save config as origin'
         setTo.text = 'Save config as destination'
         self.validatePath.text = 'Validate path'
         self.projectPath.text = "Project path"
         makePath.text = 'Create path'
         random.connect('clicked()', self.shootRandom)
+        applyConstraints.connect('clicked()', self.applyConstraints)
         setFrom.connect('clicked()', self.getFrom)
         setTo.connect('clicked()', self.getTo)
         makePath.connect('clicked()', self.makePath)
